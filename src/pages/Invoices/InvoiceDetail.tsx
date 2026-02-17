1cbab10ea50c1b857888509d1a24e47b8b3706e7
benamorasma70-coder <benamorasma70@gmail.com>
2026-02-17 22:33:45 +0100

    Add Download icon to InvoiceDetail component

--- src/pages/Invoices/InvoiceDetail.tsx
+++ src/pages/Invoices/InvoiceDetail.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { api } from '../../services/api'
-import { ArrowLeft } from 'lucide-react'
+import { ArrowLeft, Download } from 'lucide-react'
 
 interface InvoiceDetail {
   id: string
