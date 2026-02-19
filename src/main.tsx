35b8cd9f10e81d2467484db6f09a30a72d6b7257
benamorasma70-coder <benamorasma70@gmail.com>
2026-02-19 22:59:42 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import { AuthProvider } from './context/AuthContext'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ChakraProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ChakraProvider>
   </React.StrictMode>
 )
